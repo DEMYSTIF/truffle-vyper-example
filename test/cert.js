0388d342c0b924b3005ce29105107b39c7b24ff8
DEMYSTIF <mobinm.mohanan@gmail.com>
2023-04-07 11:49:36 +0530

    test_updated

--- test/cert.js
+++ test/cert.js
@@ -11,28 +11,44 @@ contract("Cert", function (accounts) {
   });
 
   it("Should issue the certificate", async function () {
-    const trx = await instance.issue(128, "Lindsey", "SRPH", "A", "2052-05-16");
+    const trx = await instance.issue(
+      128,
+      "Lindsey",
+      "SRPH",
+      "A",
+      "2052-05-16",
+      "0x2f44454d59535449462f6e6578742d63657274696669636174652d646170702f"
+    );
 
-    assert.isAtLeast(trx.receipt.blockNumber, 2);
+    const log = trx.logs[0];
+    assert.equal(log.event, "Issued");
+    assert.equal(String(log.args.id), "128");
+    assert.equal(log.args.course, "SRPH");
+    assert.equal(log.args.date, "2052-05-16");
   });
 
   it("Should read the certificate", async function () {
     const result = await instance.Certificates(128);
 
-    assert.equal(result[0], "Lindsey");
-    assert.equal(result[1], "SRPH");
-    assert.equal(result[2], "A");
-    assert.equal(result[3], "2052-05-16");
+    assert.equal(result["name"], "Lindsey");
+    assert.equal(result["course"], "SRPH");
+    assert.equal(result["grade"], "A");
+    assert.equal(result["date"], "2052-05-16");
+    assert.equal(
+      result["document"],
+      "0x2f44454d59535449462f6e6578742d63657274696669636174652d646170702f"
+    );
   });
 
   it("Should revert the transaction", async function () {
     try {
       const trx = await instance.issue(
         128,
-        "Lindsey",
+        "Abigail",
         "SRPH",
-        "A",
+        "S",
         "2052-05-16",
+        "0x2f44454d59535449462f6e6578742d63657274696669636174652d646170702f",
         { from: accounts[1] }
       );
     } catch (error) {
